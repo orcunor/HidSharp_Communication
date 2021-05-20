2e853991f2e50d7a29600b89177ff6d28a3c394e
orcun.or <orcun.or@triatech.com.tr>
2021-05-20 17:26:46 +0300

    Add project files.

---
+++ HidSharp_Form/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace HidSharp_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetAllDevices = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClearDevices = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(62, 64);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(178, 49);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(62, 150);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(178, 49);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(62, 326);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(178, 49);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(269, 392);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 38);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(62, 241);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(178, 49);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(317, 16);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(127, 30);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear Bytes";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // btnGetAllDevices
+            // 
+            this.btnGetAllDevices.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGetAllDevices.Location = new System.Drawing.Point(586, 392);
+            this.btnGetAllDevices.Name = "btnGetAllDevices";
+            this.btnGetAllDevices.Size = new System.Drawing.Size(151, 38);
+            this.btnGetAllDevices.TabIndex = 8;
+            this.btnGetAllDevices.Text = "Get All Devices!";
+            this.btnGetAllDevices.UseVisualStyleBackColor = true;
+            this.btnGetAllDevices.Click += new System.EventHandler(this.btnGetAllDevices_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(519, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(381, 311);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(20, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(205, 290);
+            this.listBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(269, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 323);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Response Bytes:";
+            // 
+            // btnClearDevices
+            // 
+            this.btnClearDevices.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClearDevices.Location = new System.Drawing.Point(755, 392);
+            this.btnClearDevices.Name = "btnClearDevices";
+            this.btnClearDevices.Size = new System.Drawing.Size(145, 38);
+            this.btnClearDevices.TabIndex = 11;
+            this.btnClearDevices.Text = "Clear Devices";
+            this.btnClearDevices.UseVisualStyleBackColor = true;
+            this.btnClearDevices.Click += new System.EventHandler(this.btnClearDevices_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 401);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 442);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClearDevices);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnGetAllDevices);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnConnect);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetAllDevices;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClearDevices;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HidSharp_Form/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HidSharp;
+
+namespace HidSharp_Form
+{
+    public partial class Form1 : Form
+    {
+        HidDevice MyDevice;
+        DeviceList List;
+        HidStream Stream;
+        IAsyncResult Result;
+        private byte[] Buffer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List = DeviceList.Local;
+                MyDevice = List.GetHidDevices(0x1781, 0x7D1).FirstOrDefault();
+                if (MyDevice != null && MyDevice.TryOpen(out Stream))
+                {
+                    Stream.ReadTimeout = 32767;
+                    textBox1.Text = MyDevice.DevicePath;
+                    btnConnect.Text = "Connected!";
+                    Buffer = new byte[9]; // 65
+                    Result = Stream.BeginRead(Buffer, 0, 9, new AsyncCallback(BytesReady), null); // 64
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Stream.EndRead(Result);
+            Stream.Dispose();
+            btnClose.Enabled = false;
+            btnOpen.Enabled = false;
+
+        }
+
+        private void BytesReady(IAsyncResult result)
+        {
+            try
+            {
+
+                label3.Text = ($"Bytes Read = {Stream.EndRead(result)} ");
+                //MessageBox.Show($"Bytes Read = {stream.EndRead(result)}");
+
+                if (listBox1.InvokeRequired)
+                    listBox1.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        for (int i = 0; i < Buffer.Length; i++)
+                        {
+                            listBox1.Items.Add($"{i}.bytes: {Buffer[i]}");
+                        }
+                         
+                    }); 
+                else
+                    for (int i = 0; i < Buffer.Length; i++)
+                    {
+                        listBox1.Items.Add(Buffer[i]);
+                    }; // BitConverter.ToString(buffer)
+
+                
+                result = Stream.BeginRead(Buffer, 0, 9, new AsyncCallback(BytesReady), null); // 64
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //byte[] BytesToSend = new byte[9];
+                byte[] testBytes = new byte[] { 0x00, 0x15, 0x00, 0x00, 0x01, 0x19, 0x00, 0x00, 0x00 }; // byte 2=1, byte 3 = 3
+                //Array.Copy(testBytes, 0, BytesToSend, 0, testBytes.Length);
+                //BytesToSend[0] = 0;
+                Stream.Write(testBytes); // ,0,65
+            }
+            catch (Exception ex)
+            {
+
+                textBox1.Text = ex.Message;
+            }
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //byte[] BytesToSend = new byte[9];
+                byte[] testBytes = new byte[] { 0x00, 0x15, 0x00, 0x00, 0x02, 0x19, 0x00, 0x00, 0x00 }; // byte 2=1, byte 3 = 3
+
+                //Array.Copy(testBytes, 0, BytesToSend, 0, testBytes.Length);
+                //BytesToSend[0] = 0;
+                Stream.Write(testBytes); // ,0,65
+            }
+            catch (Exception ex)
+            {
+
+                textBox1.Text = ex.Message;
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+       
+        private HidDevice[] GetHidDevices()
+        {
+            var list = DeviceList.Local;
+            //list.Changed += (sender, e) => Console.WriteLine("Device list changed.");
+
+            var allHidDeviceList = list.GetHidDevices().ToArray();
+            return allHidDeviceList;
+
+        }
+
+        private void btnClearDevices_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //listView1.Items.Remove(listView1.SelectedItems[0]);
+                if (listView1.Items == null)
+                {
+                    btnClearDevices.Enabled = false;
+                }
+                else
+                {
+                    listView1.Items.Clear();
+                    btnGetAllDevices.Enabled = true;
+                    btnGetAllDevices.Text = "Get All Devices!";
+                }
+            }
+            catch (Exception ex )
+            {
+
+                label2.Text = ex.Message;
+            }
+            
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count <= 0)
+            {
+                return;
+            }
+            int intselectedindex = listView1.SelectedIndices[0];
+            if (intselectedindex >= 0)
+            {
+                textBox1.Text = listView1.Items[intselectedindex].Text;
+
+            }
+        }
+
+        private void btnGetAllDevices_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                var hidDevices = GetHidDevices();
+
+                long deviceListTotalTime = stopwatch.ElapsedMilliseconds;
+
+                foreach (var device in hidDevices)
+                {
+                    listView1.Items.Add(device.ToString());
+
+                    stopwatch.Stop();
+                    listView1.Sorting = SortOrder.Ascending;
+                    label2.Text = ($"Complete device list took {deviceListTotalTime} ms to get {hidDevices.Length} devices").ToString();
+                    //listView1.Items.Add(string.Format("Max Lengths: Input {0}, Output {1}, Feature {2}",
+                    //    device.GetMaxInputReportLength(),
+                    //    device.GetMaxOutputReportLength(),
+                    //    device.GetMaxFeatureReportLength()));
+                }
+
+                //for (int i = 0; i < x.Length; i++)
+                //{
+
+                //    lstView.Items.Add(x[i].ToString());
+                //    lstView.Sorting = SortOrder.Ascending;
+                //    lblResult.Text = ($"Complete device list took {deviceListTotalTime} ms to get {x.Length} devices").ToString();
+
+                //}
+                btnGetAllDevices.Text = "Listed All Devices!";
+                btnGetAllDevices.Enabled = false;
+
+                if (listView1.Items == null)
+                {
+                    btnGetAllDevices.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
